f8643fd369e17b2ef2f5ca961f609403ab51c1ae
Francisco Victoriano <fvictoriano.dev@gmail.com>
2025-05-06 10:10:39 -0400

    error and not found pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,66 @@
+import bg from "/public/hero.jpg";
+import Image from "next/image";
+export default function NotFound() {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+      <div className="grid h-screen grid-cols-1 grid-rows-[1fr_auto_1fr] bg-white lg:grid-cols-[max(50%,36rem)_1fr]">
+        <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
+          <a href="#">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-10 w-auto sm:h-12"
+            />
+          </a>
+        </header>
+        <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+          <div className="max-w-lg">
+            <p className="text-base/8 font-semibold text-indigo-600">404</p>
+            <h1 className="mt-4 text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
+              Page not found
+            </h1>
+            <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div className="mt-10">
+              <a href="/" className="text-sm/7 font-semibold text-indigo-600">
+                <span aria-hidden="true">&larr;</span> Back to home
+              </a>
+            </div>
+          </div>
+        </main>
+        <footer className="self-end lg:col-span-2 lg:col-start-1 lg:row-start-3">
+          <div className="border-t border-gray-100 bg-gray-50 py-10">
+            <nav className="mx-auto flex w-full max-w-7xl items-center gap-x-4 px-6 text-sm/7 text-gray-600 lg:px-8">
+              <a href="#">Contact support</a>
+              <svg
+                viewBox="0 0 2 2"
+                aria-hidden="true"
+                className="size-0.5 fill-gray-300"
+              >
+                <circle r={1} cx={1} cy={1} />
+              </svg>
+              <a href="#">Status</a>
+            </nav>
+          </div>
+        </footer>
+        <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
+          <Image
+            alt=""
+            src={bg}
+            className="absolute inset-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
